b9c790ed1438ed8c3eef3c288bab86b0071664c0
Cameron Tong <cameron.tong2b@gmail.com>
2022-01-09 13:51:46 -0500

    scaffold tweets

---
+++ db/migrate/20220109185107_create_tweets.rb
@@ -0,0 +1,11 @@
+class CreateTweets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tweets do |t|
+      t.text :content
+      t.integer :likes
+      t.integer :retweets
+
+      t.timestamps
+    end
+  end
+end
