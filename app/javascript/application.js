507c030a4b769b2a9fec8fa521aad3b66db12c41
Cameron Tong <cameron.tong2b@gmail.com>
2022-01-09 13:50:30 -0500

    import jquery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,10 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import jquery from 'jquery'
+window.jQuery = jquery
+window.$ = jquery
+
+$(function(){
+  console.log('jquery is wired up')
+})
